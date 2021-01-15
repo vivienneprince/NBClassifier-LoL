80f62ee057ffaf829a56f8eb63b0b77a02b613e4
vivienneprince <wei-chun.tien13@ncf.edu>
2021-01-14 20:00:31 -0500

    complete validation section
    ok but something is wrong

--- LoL_NB_Demo.py
+++ LoL_NB_Demo.py
@@ -16,15 +16,15 @@ df = pd.read_csv("high_diamond_ranked_10min.csv")
 classes = np.sort(df['blueWins'].unique().tolist())
 
 # define features
-features = df.columns.values.tolist()
+features = [feature for feature in df.columns.values.tolist() if feature not in ('blueWins', 'gameId')]
 
 # ===================================================================
 # Split data
 # ===================================================================
 
 # create train/test subsets
-np.random.seed(2)
-mask = np.random.rand(len(df)) < 0.8
+np.random.seed(0)
+mask = np.random.rand(len(df)) < 0.9
 train = df[mask]
 test = df[~mask]
 
@@ -36,19 +36,53 @@ P_Y = []
 distributions_XGivenY = []
 
 for classtype in classes:
-    class_data = df[df['blueWins'] == classtype]
-    P_Y.append(len(class_data) / len(df))
+    class_data = train[train['blueWins'] == classtype]
+    P_Y.append(len(class_data) / len(train))
 
     for x in features:
-        if x in ['blueWins', 'gameId']:
-            continue
         feature_data = class_data[x]
-        distributions_XGivenY.append([np.mean(feature_data), np.std(feature_data)])
-
-print(P_Y)
-print(distributions_XGivenY)
+        distributions_XGivenY.append([x, np.mean(feature_data), np.std(feature_data)])
 
 # ===================================================================
 # model validation using test set
 # ===================================================================
 
+validation_array = []
+validation_score = 0
+
+
+def likelihood(value, mu, sigma):
+    a = 2 * np.pi * sigma ** 2
+    b = (value - mu) / sigma
+    return 1 / np.sqrt(a) * np.exp(-0.5 * b ** 2)
+
+
+def validate_result(guess, index):
+    if guess == test['blueWins'][index]: return 1
+    else: return 0
+
+
+for ind in test.index:
+
+    # initiate
+    P_yjGivenX = np.NINF
+    prediction = 0
+
+    for classtype in classes:
+
+        P_xiGivenyj_array = []
+
+        for feature, feature_mu, feature_sigma in distributions_XGivenY:
+            feature_value = test[feature][ind]
+            P_xiGivenyj_array.append(np.log(likelihood(feature_value, feature_mu, feature_sigma)))
+
+        temp_P = np.log(P_Y[classtype]) + np.sum(P_xiGivenyj_array)
+
+        if temp_P > P_yjGivenX:
+            P_yjGivenX = np.log(P_Y[classtype]) + np.sum(P_xiGivenyj_array)
+            prediction = classtype
+
+    validation_array.append(validate_result(prediction, ind))
+
+validation_score = np.mean(validation_array)
+print(validation_score)
