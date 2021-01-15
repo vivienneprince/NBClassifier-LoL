05e7c8a69b271edd404d81f55aae06384909b128
vivienneprince <vivie.prince@gmail.com>
2021-01-15 17:25:26 -0500

    Create extras.py

---
+++ extras.py
@@ -0,0 +1,17 @@
+# inspired by the weka output after you run a model
+# it tells you a bunch of stuff about the data and the results
+
+
+
+# TODO data name, count: datapoints, features
+
+# TODO list features
+
+# TODO table: feature (mean, sd) by class
+# TODO would like to have stats on which feature is affecting the outcome the most
+
+# TODO Model preformance data
+# accuracy by class
+# Confusion Matrix <- TODO find out what this is lol
+
+# TODO maybe some kind of visualizations
