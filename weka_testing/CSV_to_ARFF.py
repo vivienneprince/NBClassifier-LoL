ca5df06422e64ce10dbefc2275a0c7a6e98c9451
vivienneprince <vivie.prince@gmail.com>
2021-01-14 21:49:11 -0500

    complete weka test

--- weka_testing/CSV_to_ARFF.py
+++ weka_testing/CSV_to_ARFF.py
@@ -1,9 +1,9 @@
 import arff
 import pandas as pd
 
-df = pd.read_csv("high_diamond_ranked_10min.csv")
+df = pd.read_csv("toarfftest.csv")
 
-arff.dump('high_diamond_ranked_10min.arff',
+arff.dump('LoLwekaTest.arff',
           df.values,
           relation='LoLDiamondData',
           names=df.columns)
